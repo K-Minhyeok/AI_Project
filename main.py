8ce873c8c20d7f5c2f59af58ecb860eed510b458
K-Minhyeok <22000082@handong.ac.kr>
2024-05-20 09:52:29 +0900

    feat: 과거~현재 데이터 시각화 완료

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 data = pd.read_csv('temperature_data.csv', encoding='cp949')
 
-years_summer = data.iloc[8:21, 0]
-avg_temperatures = data.iloc[8:21, 2]  
-
+years_summer = data.iloc[7:21, 0]
+avg_temperatures = data.iloc[7:21, 2]
+avg_temperatures = pd.to_numeric(avg_temperatures, errors='coerce')
 
 
 plt.figure(figsize=(10, 6))
 plt.plot(years_summer, avg_temperatures, marker='o', color='b', label='Average Temperature')
 plt.xlabel('Year')
-plt.ylabel('Average Temperature')
-plt.title('Average Temperature')
+plt.ylabel('Temperature')
+plt.title('Yearly Average Temperature of Summer')
 plt.legend()
 plt.grid(True)
 plt.show()
